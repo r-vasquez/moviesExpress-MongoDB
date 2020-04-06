daf8cc91262b2c1fa6c9fe8292c3ff9fbbe441ba
Rogger <rvasque3@gmail.com>
2020-04-05 20:24:19 -0500

    adding EJS, and static files in public

---
+++ public/js/init.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let elems = document.querySelectorAll('.sidenav')
+  let instances = M.Sidenav.init(elems)
+})
+document.addEventListener('DOMContentLoaded', function () {
+  let elems2 = document.querySelectorAll('.parallax')
+  let instances2 = M.Parallax.init(elems2)
+})
